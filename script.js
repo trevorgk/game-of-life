cd08145b993870fa76ab2a210363d910d3e870c7
Trevor Kilvington <trevorgk@gmail.com>
2014-11-02 13:09:28 +1100

    Initial commit

---
+++ script.js
@@ -0,0 +1,98 @@
+"use strict";
+
+var game = (function(){
+
+	var width = 20, height = 20, tbl;
+	
+	var seed = function() {	
+		var grid = new Array(height), tr, td;
+		
+		for (var i = 0; i < height; i++) {
+			grid[i] = new Array(width);
+			tr = tbl.rows[i];
+			
+			for (var j = 0; j < width; j++) {
+				td = tr.cells[j];
+				var checkbox = td.firstChild;
+				
+				grid[i][j] = checkbox.checked;
+				
+			}
+		}
+		
+		var loop = setTimeout(function() {		
+			console.log('test');
+/*
+ * Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+ * Any live cell with two or three live neighbours lives on to the next generation.
+ * Any live cell with more than three live neighbours dies, as if by overcrowding.
+ * Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+ */
+			var previousState = grid;
+			
+			for (var i = 0; i < height; i++) {
+				for (var j = 0; j < width; j++) {
+					var liveNeighbourCount = countLiveNeighbours(grid, i, j);
+					console.log(i, j, liveNeighbourCount);
+				}
+			}
+			
+		}, 1000);
+		console.log(grid);
+	
+	};
+	
+	var countLiveNeighbours = function(grid, x, y) {
+		var count = 0;
+		for (var i = x - 1; i <= x + 1; i++) {
+			if (i < 0 || i >= height) continue;
+			for (var j = y - 1; j <= y + 1; j++) {
+				if (j < 0 || j >= width) continue;
+				if (i == x && j == y) continue;
+				
+				count += grid[i][j] ? 1 : 0;
+				
+			}
+		}
+		return count;
+	};
+	
+	var setCheckboxAt = function(x, y) {
+	}
+	
+	return {
+		init: function() {
+			console.log('init');
+			var container = document.getElementById('container'), 
+				submitBtn = document.createElement('input'),
+				tr, td, checkbox;
+			
+			tbl = document.createElement('table');
+			
+			for (var i = 0; i < height; i++) {
+				tr = document.createElement('tr');
+				
+				for (var j = 0; j < width; j++) {
+					td = document.createElement('td');
+					checkbox = document.createElement('input');
+						
+					checkbox.setAttribute('type', 'checkbox');
+					td.appendChild(checkbox);
+					tr.appendChild(td);
+				}
+				
+				tbl.appendChild(tr);
+			}
+			container.appendChild(tbl);
+			
+			submitBtn.setAttribute('type', 'submit');
+			submitBtn.setAttribute('value', 'Mindfuck!');
+			submitBtn.onclick = seed;
+			container.appendChild(submitBtn);
+			
+		}
+	}
+	
+}())
+
+game.init();
